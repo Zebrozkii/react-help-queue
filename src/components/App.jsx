3ca74255f9c5e0df406bc11a295292fc6178c6e8
Student <example@example.com>
2019-04-29 16:24:02 -0700

    working on state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
 import PicComponent from './picComponent';
 import Error404 from './Error404';
 
@@ -13,9 +13,10 @@ function App(){
       <Header/>
       <Switch>
       <Route exact path='/' component={TicketList} />
-      <Route path='/newticket' component={NewTicketForm} />
-      </Switch>
+      <Route path='/newticket' component={NewTicketControl} />
       <Route component={Error404} />
+      </Switch>
+
       <PicComponent/>
     </div>
   );
--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 
 function NewTicketForm(){
+  let _name=null;
+  let _location=null;
+  let _issue=null;
+  function handleNewTicketFormSubmission(event){
+    event.preventDefault();
+    console.log(_name.value);
+    console.log(_location.value);
+    console.log(_issue.value);
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Pair Names'
+          ref={(input) =>{_name = input;}}/>
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+          ref={(input) =>{_location = input;}}/>
         <textarea
           id='issue'
-          placeholder='Describe your issue.'/>
+          placeholder='Describe your issue.'
+          ref={(textarea) =>{_issue = textarea;}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
